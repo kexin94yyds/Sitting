3934c4e96517629b12918710dc1f527eb423ad08
kexin94yyds <your.email@example.com>
2026-04-23 09:24:00 +0800

    iterate-checkpoint:2026-04-23T01:24:00Z | 自动检查点 09:24:00
    checkpoint_id: cp_20260423012400_59e8d5aa
    turn_id: d99ebb80-e2f7-4d6d-807e-669c0f4b9f8b

--- preload.js
+++ preload.js
@@ -2,6 +2,29 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 // 暴露安全的 API 给渲染进程
 contextBridge.exposeInMainWorld('electronAPI', {
+  // 新状态机 API
+  getReminderState: () => ipcRenderer.invoke('get-reminder-state'),
+
+  subscribeReminderState: (callback) => {
+    const listener = (event, state) => callback(state);
+    ipcRenderer.on('reminder-state', listener);
+    return () => ipcRenderer.removeListener('reminder-state', listener);
+  },
+
+  updateReminderSettings: (settings) => ipcRenderer.invoke('update-reminder-settings', settings),
+
+  startTracking: () => ipcRenderer.invoke('start-tracking'),
+
+  pauseTracking: () => ipcRenderer.invoke('pause-tracking'),
+
+  stopTracking: () => ipcRenderer.invoke('stop-tracking'),
+
+  startBreak: () => ipcRenderer.invoke('start-break'),
+
+  snooze: (minutes) => ipcRenderer.invoke('snooze', minutes),
+
+  skipOnce: () => ipcRenderer.invoke('skip-once'),
+
   // 获取设置
   getSettings: () => ipcRenderer.invoke('get-settings'),
   
@@ -35,6 +58,5 @@ contextBridge.exposeInMainWorld('electronAPI', {
     ipcRenderer.removeAllListeners(channel);
   }
 });
-
 
 
